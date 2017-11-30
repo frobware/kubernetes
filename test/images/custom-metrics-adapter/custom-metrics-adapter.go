548e6ebbc30bb1e7a91730f0129016f41a01b306
Andrew McDermott <aim@frobware.com>
2017-11-30 11:40:03 +0000

    Dump custom-metrics-adapter
    (to be renamed)

--- test/images/custom-metrics-adapter/custom-metrics-adapter.go
+++ test/images/custom-metrics-adapter/custom-metrics-adapter.go
@@ -17,10 +17,103 @@ limitations under the License.
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"strconv"
+	"time"
 )
 
+var (
+	port = flag.Int("port", 80, "Port number.")
+
+	metrics = []metric{
+		{"packets", time.Second, 0},
+		{"queries", time.Second, 0},
+	}
+)
+
+type metric struct {
+	Name  string
+	Rate  time.Duration // XXX remove
+	Value int64
+}
+
+func (m metric) String() string {
+	return fmt.Sprintf("%s %v", m.Name, m.Value)
+}
+
+func logRequest(req *http.Request) {
+	if dreq, err := httputil.DumpRequest(req, true); err != nil {
+		log.Println(err)
+	} else {
+		log.Println(string(dreq))
+	}
+}
+
+func writeText(w http.ResponseWriter, s string) {
+	log.Println(s)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(s))
+	w.Write([]byte("\r\n"))
+}
+
+func dumpMetrics(metrics []metric) {
+	for _, m := range metrics {
+		log.Println(m)
+	}
+}
+
+func updateMetric(value string, m *metric) error {
+	if value == "" {
+		return nil
+	}
+	n, err := strconv.ParseInt(value, 10, 0)
+	if err != nil {
+		return err
+	}
+	m.Value = n
+	return nil
+}
+
+func serveMetrics(w http.ResponseWriter, req *http.Request) {
+	logRequest(req)
+
+	w.Header().Set("Content-Type", "text/plain")
+
+	for _, m := range metrics {
+		w.Write([]byte(fmt.Sprintf("%s\n", m)))
+	}
+}
+
+func serveRoot(w http.ResponseWriter, req *http.Request) {
+	logRequest(req)
+
+	if req.Method != "POST" {
+		return
+	}
+
+	if err := req.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i := range metrics {
+		if err := updateMetric(req.FormValue(metrics[i].Name), &metrics[i]); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	dumpMetrics(metrics)
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.SetFlags(log.Flags() | log.Lmicroseconds)
+	flag.Parse()
+	http.HandleFunc("/metrics", serveMetrics)
+	http.HandleFunc("/", serveRoot)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
